87046733ca01f9223066af44da1ff7f53e4382ff
Ramon Victor <ramon.victor@t-online.de>
2016-06-01 20:57:17 +0200

    Anpassung für Release v.1.1

--- src/main/java/de/victorfx/audipic/Main.java
+++ src/main/java/de/victorfx/audipic/Main.java
@@ -15,7 +15,7 @@ import java.net.URL;
  */
 public class Main extends Application {
 
-    public static final String VERSION = "1.1-SNAPSHOT";
+    public static final String VERSION = "1.1";
 
     public static void main(String[] args) {
         launch(args);
